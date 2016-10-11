46877ce7475809fdb444c766137a2cc0af3083cb
Tyler C <tyler.corum@gmail.com>
2016-10-11 00:26:29 -0700

    whew, worked out some bugs but the backend is functional.  next is sorting and displaying unique top-fastest racers and a GUI

--- problemChild.py
+++ problemChild.py
@@ -26,7 +26,7 @@
 #
 # /$\ # urgent and important
 # /!\ # important
-# /;\ # urgency implied but not important
+# /#\ # important but not ugent
 # /?\ # error?/unknown?
 
 
@@ -138,9 +138,12 @@ root.mainloop()
 # button.config(command = callback)
 # button.state(['disabled'])
 
+import random
 from random import randint
 from random import random
 from random import randrange
+from random import uniform
+import pdb
 
 horseData = [
 	["Alpha",None],
@@ -180,6 +183,7 @@ horseData = [
     ["Yoshi",None]
     ]
 
+winstreak = [None,None,None,None,None,]
 
 # /!\ # I am going to have to change rand 0,4  to round(random()) to see if the same horse wins again because otherwise it's too much randomness!!!
 
@@ -188,55 +192,100 @@ horseData = [
 randomWinnerThisRace = None
 count = 0
 
-firstRaceWinner = (randrange(5))
+raceWinner = (randrange(5))
 
 for raceNumber in range(0,6,1): # race loop, race 6 times: race# 0,1,2,3,4,5
-	print('In raceNumber for loop, race#{}, count={}, randWinTR:{}'.format(raceNumber, count,firstRaceWinner))
-	# firstRaceWinner = 
-	print('randWinTR after randmoizer:{}'.format(firstRaceWinner))
+	print('In raceNumber begging, race#{}, count={}'.format(raceNumber,count))
+	if raceNumber == 5:
+		pdb.set_trace()
+	# raceWinner = 
+	# print('randWinner after randmizer:{}'.format(raceWinner))
 
 	#### start first race... just the first race only
 	if raceNumber == 0: 
 		for theHorse in range(0,5): # horse 0,1,2,3,4 (alpha,brigadier, chutzpah, dragonfly,excalibur)
 			print('Horse being assessed:{}'.format(horseData[count][0]))
-			print('In theHorse for loop, count={}'.format(count))
+			# print('In theHorse for loop, count={}'.format(count))
 			
-			if theHorse == firstRaceWinner:
+			if theHorse == raceWinner:
+				horseData[count][1] = 1.0 	# set the winner's distance to 1.0    // I could just do this before the first race after firstRaceWinner
 				horseData[5][0] = horseData[count][0] 	# update the data table, pos[5] races in the next race
 				horseData[5][1] = horseData[count][1]	# carry their distance over too		   // I just realized this is overcomplicating things
-				horseData[count][1] = 1.0 				# set the winner's distance to 1.0    // I could just do this before the first race after firstRaceWinner
-				print('Current Fastest: {}'.format(horseData[count][1]))
+				print('{}\'s distance in race{}: {}'.format(horseData[count][0], raceNumber, horseData[count][1]))
+				count += 1
 
 			else:
-				horseData[count][1] = (random.uniform(.500, .999))
-			
-			count += 1
+				horseData[count][1] = (uniform(.700, .999))
+				print('{}\'s distance in race{}: {}'.format(horseData[count][0], raceNumber, horseData[count][1]))
+				count += 1
 	#### end  first race
 
-	horseWinsAgain = round(random())	
-	if horseWinsAgain == 1:
-		print('\nHorse {} WINS AGAIN!\n'.format(horseWinsAgain))
+	# /#\ # pdb.set_trace()
+
+	#if wonAgain == 1:
+	#	print('\nHorse {} WINS AGAIN!\n'.format(wonAgain)) # this is weird right now.  race has to finish first
 
 	#if at least 1 race has been ran and all 6 races aren't over, assess whether same winner and adj data accordingly
-	if count in (10, 15, 20, 25, 30) and horseWinsAgain == 1: 	# 1 == True
-		for adjRelatively in range(0,(count-5)): 	# count-5 because the current count is the current race, -5 takes it to the previous race(s)
-			horseData[adjRelatively][1] = (horseData[adjRelatively][1]*(horseData[count-5][1]))
+	if count in (10, 15, 20, 25, 30) and horseData[count] != horseData[count-5]: 	# if these were equal it would mean the same horse won and thus no relative distance change
+		#pdb.set_trace()
+		for recurseAdj in range(0,(count-5)): 	# count-5 because the current count is the current race, -5 takes it to the previous race(s)
+			horseData[recurseAdj][1] = (horseData[recurseAdj][1]*(horseData[count-5][1]))
 	
-	#### start second, etc race analysis
-	if raceNumber > 0: # second, third, etc races
-		for theHorse in range(0,5):
-			if horseWinsAgain == 1:
-				theHorse += 1
-				if raceNumber == 5:
-					# horseData[count][1] = 1.0
-					print('Last Race...') #don't know what else to put here
-				else:
-					horseData[5*(raceNumber+1)][0] = horseData[count][0] 	# update the data table, pos[5] races in the next race
-					horseData[5][1] = horseData[count][1]	# carry their distance over too		   // I just realized this is overcomplicating things
-			else:
-				horseData[count][1] = (random.uniform(.500, .999))
+	if raceNumber != 5:
+		wonAgain = round(random())	#0 = no, 1 = yes
+		winstreak[raceNumber] = wonAgain
+		if wonAgain != 1:
+			print('horse //{}// didn\'t win again'.format(horseData[5*(raceNumber + 1)]))
+			print('current count: {}'.format(count))
 
-			count += 1
+	#### start second, third etc race analysis
+	#if raceNumber > 0: # second, third, etc races
+	if raceNumber != 0 and raceNumber != 5 and wonAgain == 1:
+		# horseData[count][1] = 1.0 # set 5, 10, 15, 20, 25 as 1.0 (these are the winners)
+		horseData[5*(raceNumber+1)][0] = horseData[count][0] # place winner into next race
+		horseData[5*(raceNumber+1)][1] = 1.0 #set distance of next race = 1.0 //not explicitly needed has to check if wonAgain
+		count += 1 # count 1 for the winning horse to move the analysis to the next horse
+		for theHorse in range(1,5): # +1,+2,+3,+4 horses; 
+			horseData[count][1] = (uniform(.700, .999)) # count equals the current analysis position
+			print('{}\'s distance in race{}: {}'.format(horseData[count][0], raceNumber, horseData[count][1]))
+			count += 1		# next
+
+
+	elif raceNumber != 0 and raceNumber != 5 and wonAgain != 1: # if wonAgain != 1/if horseHasToBattleAgainForTopPosition
+		raceWinner = (randrange(5)) # choose a random winner, could be same horse
+		for theHorse in range(0,5):
+			if theHorse == raceWinner:
+				horseData[5*(raceNumber+1)][0] = horseData[count][0] #5*raceNum+1 = place winner into next race
+				horseData[5*(raceNumber+1)][1] = 1.0 #set distance of next race = 1.0 //not needed has to check if wonAgain
+				horseData[count][1] = 1.0
+				count += 1
+			else:
+				horseData[count][1] = (uniform(.700, .999))
+				print('{}\'s distance in race{}: {}'.format(horseData[count][0], raceNumber, horseData[count][1]))
+				count += 1
+
+	elif raceNumber == 5 and wonAgain == 1: #6th race, last race
+		count += 1
+		for theHorse in range(1,5): # +1,+2,+3,+4 horses; 
+			horseData[count][1] = (uniform(.700, .999)) # count equals the current analysis position
+			print('{}\'s distance in race{}: {}'.format(horseData[count][0], raceNumber, horseData[count][1]))
+			count += 1		# next
+
+	elif raceNumber == 5 and wonAgain != 1: # 6thracenumber == 5 and horsewinsagain != 1
+		raceWinner = (randrange(5)) # choose a random winner, could be same horse
+		for theHorse in range(0,5):
+			if theHorse == raceWinner:
+				horseData[count][1] = 1.0
+				count += 1
+			else:
+				horseData[count][1] = (uniform(.700, .999))
+				print('{}\'s distance in race{}: {}'.format(horseData[count][0], raceNumber, horseData[count][1]))
+				count += 1
+
+		if count == 30 and horseData[count] != horseData[count-5]: 	# if these were equal it would mean the same horse won and thus no relative distance change
+			pdb.set_trace()
+			for recurseAdj in range(0,(count-5)): 	# count-5 because the current count is the current race, -5 takes it to the previous race(s)
+				horseData[recurseAdj][1] = (horseData[recurseAdj][1]*(horseData[count-5][1]))
 
 
 
@@ -270,8 +319,8 @@ print('\nTop Horse: {}:{}\nSecond: {}:{}\nThird: {}:{}\n'.format(horseData[29][0
 
 
 
-horseWinsAgain = round(random())
-print(horseWinsAgain)
+wonAgain = round(random())
+print(wonAgain)
 
 
 
--- testing.py
+++ testing.py
@@ -1,6 +1,9 @@
+import random
 from random import randint
 from random import random
 from random import randrange
+from random import uniform
+import pdb
 
 horseData = [
 	["Alpha",None],
@@ -40,6 +43,7 @@ horseData = [
     ["Yoshi",None]
     ]
 
+winstreak = [None,None,None,None,None,]
 
 # /!\ # I am going to have to change rand 0,4  to round(random()) to see if the same horse wins again because otherwise it's too much randomness!!!
 
@@ -47,31 +51,98 @@ horseData = [
 
 randomWinnerThisRace = None
 count = 0
-for j in range(0,6,1):
-	print('In j for loop, race#{}, count={}, randWinTR:{}'.format(j, count,randomWinnerThisRace))
-	randomWinnerThisRace = (randrange(5))
-	print('randWinTR after randmoizer:{}'.format(randomWinnerThisRace))
 
-	#if j > 0 and j < 6 and randomWinnerThisRace == 0: # assess whether same winner and adj data accordingly
-	for i in range(0,5,1):
-		print('Horse being assessed:{}'.format(horseData[count][0]))
-		print('In i for loop, count={}'.format(count))
-		if i == randomWinnerThisRace:
-			if j == 5: # reserved if stmt for last race so don't place winner up against ghosts :)
+raceWinner = (randrange(5))
+
+for raceNumber in range(0,6,1): # race loop, race 6 times: race# 0,1,2,3,4,5
+	print('In raceNumber begging, race#{}, count={}'.format(raceNumber,count))
+	if raceNumber == 5:
+		pdb.set_trace()
+	# raceWinner = 
+	# print('randWinner after randmizer:{}'.format(raceWinner))
+
+	#### start first race... just the first race only
+	if raceNumber == 0: 
+		for theHorse in range(0,5): # horse 0,1,2,3,4 (alpha,brigadier, chutzpah, dragonfly,excalibur)
+			print('Horse being assessed:{}'.format(horseData[count][0]))
+			# print('In theHorse for loop, count={}'.format(count))
+			
+			if theHorse == raceWinner:
+				horseData[count][1] = 1.0 	# set the winner's distance to 1.0    // I could just do this before the first race after firstRaceWinner
+				horseData[5][0] = horseData[count][0] 	# update the data table, pos[5] races in the next race
+				horseData[5][1] = horseData[count][1]	# carry their distance over too		   // I just realized this is overcomplicating things
+				print('{}\'s distance in race{}: {}'.format(horseData[count][0], raceNumber, horseData[count][1]))
+				count += 1
+
+			else:
+				horseData[count][1] = (uniform(.700, .999))
+				print('{}\'s distance in race{}: {}'.format(horseData[count][0], raceNumber, horseData[count][1]))
+				count += 1
+	#### end  first race
+
+	# /#\ # pdb.set_trace()
+
+	#if wonAgain == 1:
+	#	print('\nHorse {} WINS AGAIN!\n'.format(wonAgain)) # this is weird right now.  race has to finish first
+
+	#if at least 1 race has been ran and all 6 races aren't over, assess whether same winner and adj data accordingly
+	if count in (10, 15, 20, 25, 30) and horseData[count] != horseData[count-5]: 	# if these were equal it would mean the same horse won and thus no relative distance change
+		#pdb.set_trace()
+		for recurseAdj in range(0,(count-5)): 	# count-5 because the current count is the current race, -5 takes it to the previous race(s)
+			horseData[recurseAdj][1] = (horseData[recurseAdj][1]*(horseData[count-5][1]))
+	
+	if raceNumber != 5:
+		wonAgain = round(random())	#0 = no, 1 = yes
+		winstreak[raceNumber] = wonAgain
+		if wonAgain != 1:
+			print('horse //{}// didn\'t win again'.format(horseData[5*(raceNumber + 1)]))
+			print('current count: {}'.format(count))
+
+	#### start second, third etc race analysis
+	#if raceNumber > 0: # second, third, etc races
+	if raceNumber != 0 and raceNumber != 5 and wonAgain == 1:
+		# horseData[count][1] = 1.0 # set 5, 10, 15, 20, 25 as 1.0 (these are the winners)
+		horseData[5*(raceNumber+1)][0] = horseData[count][0] # place winner into next race
+		horseData[5*(raceNumber+1)][1] = 1.0 #set distance of next race = 1.0 //not explicitly needed has to check if wonAgain
+		count += 1 # count 1 for the winning horse to move the analysis to the next horse
+		for theHorse in range(1,5): # +1,+2,+3,+4 horses; 
+			horseData[count][1] = (uniform(.700, .999)) # count equals the current analysis position
+			print('{}\'s distance in race{}: {}'.format(horseData[count][0], raceNumber, horseData[count][1]))
+			count += 1		# next
+
+
+	elif raceNumber != 0 and raceNumber != 5 and wonAgain != 1: # if wonAgain != 1/if horseHasToBattleAgainForTopPosition
+		raceWinner = (randrange(5)) # choose a random winner, could be same horse
+		for theHorse in range(0,5):
+			if theHorse == raceWinner:
+				horseData[5*(raceNumber+1)][0] = horseData[count][0] #5*raceNum+1 = place winner into next race
+				horseData[5*(raceNumber+1)][1] = 1.0 #set distance of next race = 1.0 //not needed has to check if wonAgain
 				horseData[count][1] = 1.0
-			else: # do this when assessing winner:
-				horseData[5*(j+1)][0] = horseData[count][0]
-				horseData[5*(j+1)][1] = horseData[count][1]
-				horseData[count][1] = 1.0
-				print('Current Fastest: {}'.format(horseData[count][1]))
-		else:
-			horseData[count][1] = (random())
+				count += 1
+			else:
+				horseData[count][1] = (uniform(.700, .999))
+				print('{}\'s distance in race{}: {}'.format(horseData[count][0], raceNumber, horseData[count][1]))
+				count += 1
+
+	elif raceNumber == 5 and wonAgain == 1: #6th race, last race
 		count += 1
-		if count in (10, 15, 20, 25, 30) and randomWinnerThisRace != 0:
-			for adjRelativeDist in range(0,(count-5)):
-				# adjRatio = [5*(j+1)]
-				horseData[adjRelativeDist][1] = (horseData[adjRelativeDist][1]*(horseData[count-5][1]))
+		for theHorse in range(1,5): # +1,+2,+3,+4 horses; 
+			horseData[count][1] = (uniform(.700, .999)) # count equals the current analysis position
+			print('{}\'s distance in race{}: {}'.format(horseData[count][0], raceNumber, horseData[count][1]))
+			count += 1		# next
 
-horseData.sort(key=lambda pair: pair[1])
-# for top3 in range(4):
-enumerate(horseData)
+	elif raceNumber == 5 and wonAgain != 1: # 6thracenumber == 5 and horsewinsagain != 1
+		raceWinner = (randrange(5)) # choose a random winner, could be same horse
+		for theHorse in range(0,5):
+			if theHorse == raceWinner:
+				horseData[count][1] = 1.0
+				count += 1
+			else:
+				horseData[count][1] = (uniform(.700, .999))
+				print('{}\'s distance in race{}: {}'.format(horseData[count][0], raceNumber, horseData[count][1]))
+				count += 1
+
+		if count == 30: 	# if these were equal it would mean the same horse won and thus no relative distance change
+			# pdb.set_trace()
+			for recurseAdj in range(0,(count-5)): 	# count-5 because the current count is the current race, -5 takes it to the previous race(s)
+				horseData[recurseAdj][1] = (horseData[recurseAdj][1]*(horseData[count-5][1]))
